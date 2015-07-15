668c5defeac8eded352c1383c27a859ca1f8db9e
alex <alex@nabu.be>
2015-07-15 08:51:22 +0200

    Added peer certificates to security header

--- src/main/java/be/nabu/libs/http/api/server/SecurityHeader.java
+++ src/main/java/be/nabu/libs/http/api/server/SecurityHeader.java
@@ -1,9 +1,12 @@
 package be.nabu.libs.http.api.server;
 
+import java.security.cert.Certificate;
+
 import javax.net.ssl.SSLContext;
 
 import be.nabu.utils.mime.api.Header;
 
 public interface SecurityHeader extends Header {
 	public SSLContext getSecurityContext();
+	public Certificate [] getPeerCertificates();
 }
