2e70407cb74959253403ebc577b2016a5241b744
alex <alex@nabu.be>
2015-08-14 09:55:23 +0200

    Updated interface to be compatible with dynamic resources as needed by the broker

--- src/main/java/be/nabu/libs/http/api/server/MessageDataProvider.java
+++ src/main/java/be/nabu/libs/http/api/server/MessageDataProvider.java
@@ -2,7 +2,8 @@ package be.nabu.libs.http.api.server;
 
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.WritableResource;
+import be.nabu.utils.mime.api.Header;
 
 public interface MessageDataProvider {
-	public <T extends WritableResource & ReadableResource> T newResource(Long size);
+	public <T extends WritableResource & ReadableResource> T newResource(String request, Header...headers);
 }
