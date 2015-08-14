ac556ea419e5b2e7928bdf3e554d59393babf924
alex <alex@nabu.be>
2015-08-14 18:39:33 +0200

    Added exception

--- src/main/java/be/nabu/libs/http/api/server/MessageDataProvider.java
+++ src/main/java/be/nabu/libs/http/api/server/MessageDataProvider.java
@@ -1,9 +1,11 @@
 package be.nabu.libs.http.api.server;
 
+import java.io.IOException;
+
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.WritableResource;
 import be.nabu.utils.mime.api.Header;
 
 public interface MessageDataProvider {
-	public <T extends WritableResource & ReadableResource> T newResource(String request, Header...headers);
+	public <T extends WritableResource & ReadableResource> T newResource(String request, Header...headers) throws IOException;
 }
