7a52b661b11a49fcb2920371b09e090a8ae566a8
alex <alex@nabu.be>
2015-11-02 20:39:59 +0100

    Removed deprecated interface

--- src/main/java/be/nabu/libs/http/api/server/HTTPExceptionFormatter.java
+++
@@ -1,9 +0,0 @@
-package be.nabu.libs.http.api.server;
-
-import be.nabu.libs.http.HTTPException;
-import be.nabu.libs.http.api.HTTPRequest;
-import be.nabu.libs.http.api.HTTPResponse;
-
-public interface HTTPExceptionFormatter {
-	public HTTPResponse format(HTTPRequest request, HTTPException e);
-}
