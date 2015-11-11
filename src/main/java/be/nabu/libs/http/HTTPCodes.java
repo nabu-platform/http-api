ace15f5f5c6780a10aa742f5d2a774c0dd3f4094
alex <alex@nabu.be>
2015-11-11 17:08:45 +0100

    Added status code 426

--- src/main/java/be/nabu/libs/http/HTTPCodes.java
+++ src/main/java/be/nabu/libs/http/HTTPCodes.java
@@ -38,6 +38,7 @@ public class HTTPCodes {
 			case 415: return "Unsupported Media Type";
 			case 416: return "Requested Range Not Satisfiable";
 			case 417: return "Expectation Failed";
+			case 426: return "Upgrade Required";
 			case 500: return "Internal Server Error";
 			case 501: return "Not Implemented";
 			case 502: return "Bad Gateway";
