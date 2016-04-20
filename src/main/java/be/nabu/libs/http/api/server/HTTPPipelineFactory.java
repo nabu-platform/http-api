f9752dcbc09c67e668cc43ad041fce51ec145310
alex <alex@nabu.be>
2016-04-20 07:43:45 +0200

    Added getters/setters for configurable properties

--- src/main/java/be/nabu/libs/http/api/server/HTTPPipelineFactory.java
+++ src/main/java/be/nabu/libs/http/api/server/HTTPPipelineFactory.java
@@ -6,4 +6,12 @@ public interface HTTPPipelineFactory extends PipelineFactory {
 	public HTTPProcessorFactory getProcessorFactory();
 	public MessageDataProvider getMessageDataProvider();
 	public void setMessageDataProvider(MessageDataProvider messageDataProvider);
+	public long getReadTimeout();
+	public void setReadTimeout(long timeout);
+	public long getWriteTimeout();
+	public void setWriteTimeout(long timeout);
+	public int getRequestLimit();
+	public void setRequestLimit(int limit);
+	public int getResponseLimit();
+	public void setResponseLimit(int limit);
 }
