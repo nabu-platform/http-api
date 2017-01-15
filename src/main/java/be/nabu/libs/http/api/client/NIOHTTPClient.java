581c787d0f96dd25ff3b42fc61bf940cb77fa9c9
Alexander Verbruggen <alex@nabu.be>
2017-01-15 17:49:04 +0100

    Renamed interface to be more in line with server naming

--- src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
+++ src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
@@ -1,5 +1,6 @@
 package be.nabu.libs.http.api.client;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.concurrent.Future;
@@ -9,7 +10,7 @@ import be.nabu.libs.http.api.HTTPRequest;
 import be.nabu.libs.http.api.HTTPResponse;
 import be.nabu.utils.mime.impl.FormatException;
 
-public interface NonBlockingHTTPClient extends HTTPClient {
+public interface NIOHTTPClient extends HTTPClient, Closeable {
 	public Future<HTTPResponse> call(HTTPRequest request, boolean secure) throws IOException, FormatException, ParseException;
 	public EventDispatcher getDispatcher();
 }
