b6b99cf5a578cf8b6bf04bfa23c3649379a3e4b9
Alexander Verbruggen <alex@nabu.be>
2017-04-23 15:19:13 +0200

    Remove the event dispatcher from the interface

--- src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
+++ src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.concurrent.Future;
 
-import be.nabu.libs.events.api.EventDispatcher;
 import be.nabu.libs.http.api.HTTPRequest;
 import be.nabu.libs.http.api.HTTPResponse;
 import be.nabu.utils.mime.impl.FormatException;
 
 public interface NIOHTTPClient extends HTTPClient, Closeable {
 	public Future<HTTPResponse> call(HTTPRequest request, boolean secure) throws IOException, FormatException, ParseException;
-	public EventDispatcher getDispatcher();
 }
