faa6713ed9b552bd5aef4f56a3e6495fdca319ce
Alexander Verbruggen <alex@nabu.be>
2017-06-19 09:36:09 +0200

    Make all http clients closeable for generic closing

--- src/main/java/be/nabu/libs/http/api/client/HTTPClient.java
+++ src/main/java/be/nabu/libs/http/api/client/HTTPClient.java
@@ -1,5 +1,6 @@
 package be.nabu.libs.http.api.client;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.security.Principal;
 import java.text.ParseException;
@@ -8,6 +9,6 @@ import be.nabu.libs.http.api.HTTPRequest;
 import be.nabu.libs.http.api.HTTPResponse;
 import be.nabu.utils.mime.impl.FormatException;
 
-public interface HTTPClient {
+public interface HTTPClient extends Closeable {
 	public HTTPResponse execute(HTTPRequest request, Principal principal, boolean secure, boolean followRedirects) throws IOException, FormatException, ParseException;
 }
--- src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
+++ src/main/java/be/nabu/libs/http/api/client/NIOHTTPClient.java
@@ -1,6 +1,5 @@
 package be.nabu.libs.http.api.client;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.concurrent.Future;
@@ -9,6 +8,6 @@ import be.nabu.libs.http.api.HTTPRequest;
 import be.nabu.libs.http.api.HTTPResponse;
 import be.nabu.utils.mime.impl.FormatException;
 
-public interface NIOHTTPClient extends HTTPClient, Closeable {
+public interface NIOHTTPClient extends HTTPClient {
 	public Future<HTTPResponse> call(HTTPRequest request, boolean secure) throws IOException, FormatException, ParseException;
 }
